3713817f5eb4065561288e384a43ff86e44dbd60
ppowers <penelope.powers@treefin.com>
2023-05-04 15:12:05 +0200

    WW-2118 change hasOwnProperty usage

--- hooks/lib/configXmlParser.js
+++ hooks/lib/configXmlParser.js
@@ -51,7 +51,8 @@ function readPreferences(cordovaContext) {
 // region Private API
 
 function getTeamIdPreference(xmlPreferences) {
-  if (xmlPreferences.hasOwnProperty('ios-team-id')) {
+  if (xmlPreferences.hasOwnProperty('ios-'
+      + 'team-id')) {
     return xmlPreferences['ios-team-id'][0]['$']['value'];
   }
 
